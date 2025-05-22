1990db8b4b53ac71be8762e981bbc2c6c670f387
ivan-lesh <leshokivan2005@gmail.com>
2025-05-22 21:31:06 +0300

    1

--- views/catalog/category.php
+++ views/catalog/category.php
@@ -6,7 +6,7 @@ use yii\helpers\VarDumper;
 
 ?>
 <div class="card h-100 <?= isset($model->parent_id) ? "rounded-4 card-category" : 'border-0' ?>">
-    <div class="card-body p-0 <?= isset($model->parent_id) ? "border-0" : '' ?>">
+    <div class="card-body h-100 p-0 <?= isset($model->parent_id) ? "border-0" : '' ?>">
         <?php
             echo Html::a(
                 (isset($model->parent_id)
@@ -14,9 +14,9 @@ use yii\helpers\VarDumper;
                 : ''
                 ) . 
                 "<h5 class='card-title ellipsis" . (isset($model->parent_id) ? 
-                " fs-6 mb-0 fw-medium" : ' border-bottom border-3 border-danger pe-2') . "'>$model->title</h5>",
+                " fs-6 mb-0 fw-medium" : ' border-bottom border-3 border-danger') . "'>$model->title</h5>",
                 ['view', 'id' => $model->id],
-                ['class' => 'd-flex gap-2 px-2 py-1 h-100 ' . (isset($model->parent_id) ? 'justify-content-center' : '') . ' align-items-center link-dark text-decoration-none px-2', 'data-pjax' => 0]
+                ['class' => 'd-flex gap-2 p-2 h-100 ' . (isset($model->parent_id) ? 'justify-content-center' : '') . ' align-items-center link-dark text-decoration-none', 'data-pjax' => 0]
             )
         ?>
 
