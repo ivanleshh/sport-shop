66a8b4a754ffc75573b12b5bfa8bcfab0a0ff961
ivan-lesh <leshokivan2005@gmail.com>
2025-06-07 03:20:11 +0300

    123

--- config/params.php
+++ config/params.php
@@ -6,6 +6,4 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'bsVersion' => '5.x',
-    'stripe.secret' => 'sk_test_51RX4FqR7ivPvUJojPZRkdN4emyiCO0WWrErvnoUmWNKBS5Ghs5RnRsGXVstQ2h6LGvlt9shpzk2yFvM6UqUce0Uo005iFiCvbJ',
-    'stripe.publishable' => 'pk_test_51RX4FqR7ivPvUJojTt0hE3zIOJxdmkEmCUfsKXfwKsExTQsnYbOedDncYR76z8GHgqTHGH36ap9cugPbI3rYOYAQ00nSnRwv3p',
 ];
