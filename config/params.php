498da13068c357ee2da37132dd8635352d1b86a4
ivan-lesh <leshokivan2005@gmail.com>
2025-06-07 03:22:20 +0300

    123

--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'bsDependencyEnabled' => false,
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'bsVersion' => '5.x',
-];
